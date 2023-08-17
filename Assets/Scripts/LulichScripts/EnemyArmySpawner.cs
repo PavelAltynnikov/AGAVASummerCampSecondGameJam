9f7fe6165be00fee4d8c4f6d89c07f8a8748a1b1
Lul1ch <84387626+Lul1ch@users.noreply.github.com>
2023-08-17 11:03:45 +0300

    Create enemy army spawn script, update scene

---
+++ Assets/Scripts/LulichScripts/EnemyArmySpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArmySpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemiesPrefabs;
+
+    [Header("Spawning parameters")]
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private float XAxisStep;
+    [SerializeField] private float ZAxisStep;
+    [SerializeField] private int maxEnemiesInRow;
+    [SerializeField] private int overallEnemiesNumber;
+
+    private Vector3 currentPos;
+    private int currEnemiesNumberInRow;
+
+    private void Awake() {
+        currentPos = startPos;
+
+        StartCoroutine("SpawnEnemies");
+    }
+
+    private IEnumerator SpawnEnemies() {
+        while(overallEnemiesNumber > 0) {
+            if (currEnemiesNumberInRow == maxEnemiesInRow) {
+                currentPos.z -= ZAxisStep;
+                currentPos.x = startPos.x;
+                currEnemiesNumberInRow = 0;
+            }
+            int rand = Random.Range(0, enemiesPrefabs.Count);
+            Instantiate(enemiesPrefabs[rand], currentPos, Quaternion.identity);
+            currentPos.x += XAxisStep;
+            currEnemiesNumberInRow++;
+            overallEnemiesNumber--;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
