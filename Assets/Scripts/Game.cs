250339a7c8968e7a8fa98e88faf6431bdd12c2a6
PavelAltynnikov <robenzon11@gmail.com>
2023-08-17 21:54:07 +0400

    move armies

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -26,6 +26,7 @@ public class Game : MonoBehaviour
     [SerializeField] private Leaderboard _totalScoreLeaderboard;
     [SerializeField] private LeanToken _levelToken;
     [SerializeField] private Transform _battlePoint;
+    [SerializeField] private EnemyArmySpawner _enemySpawner;
 
     private Aviary _lastAviary;
     private int _level;
@@ -202,14 +203,8 @@ public class Game : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
 
         // TODO вот тут перед выводом UI надо запускать войска.
-        foreach (Aviary aviary in _aviaries)
-        {
-            foreach (Animal animal in aviary.Animals)
-            {
-                Vector3 targetPoint = _battlePoint.position + animal.transform.position;
-                animal.Go(targetPoint, 1f);
-            }
-        }
+        StartCoroutine(nameof(MoveOurArmy), _battlePoint.position);
+        _enemySpawner.MoveEnemyArmy(_battlePoint.position);
 
         _doneScreen.Appear(_score.Value, _level);
         DB.AddScore(_score.Value);
@@ -225,6 +220,20 @@ public class Game : MonoBehaviour
             else
                 Agava.YandexGames.Leaderboard.SetScore(_totalScoreLeaderboard.Name, _score.Value);
         });
+    }
+
+    private void MoveOurArmy(Vector3 endPoint)
+    {
+        var offset = new Vector3(0, 0, 20);
+
+        foreach (Aviary aviary in _aviaries)
+        {
+            foreach (Animal animal in aviary.Animals)
+            {
+                Vector3 targetPoint = endPoint + offset + animal.transform.position;
+                animal.Go(targetPoint, 2f);
+            }
+        }
     }
 
     private IEnumerator MovePlusText(PopupText plusText, float delay, int score)
